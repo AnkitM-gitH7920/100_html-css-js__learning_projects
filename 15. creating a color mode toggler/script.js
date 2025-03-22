1e010400ca35a205c88030859df7b67f6fcab01f
AnkitM-gitH7920 <ankitmehra7920@gmail.com>
2025-03-22 11:13:39 +0530

    added more files

--- 15. creating a color mode toggler/script.js
+++
@@ -1,6 +0,0 @@
-let toggler = document.getElementById('switch');
-toggler.addEventListener("click",(event)=>{
-	toggler.checked
-	? document.body.style.background = "black"
-	: document.body.style.background = "white"
-})
