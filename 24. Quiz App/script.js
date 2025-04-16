19f20e7604db647af6980c74bb57f3af7c6c1581
AnkitM-gitH7920 <ankitmehra7920@gmail.com>
2025-04-16 11:51:17 +0530

    added weather app

--- 24. Quiz App/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("submit");
-const btnTxt = document.querySelector(".btnText");
-const answers = document.querySelectorAll(".ans");
-const questionDisplay = document.querySelector(".question");
-
-let questionIndex = 0;
-let score = 0;
